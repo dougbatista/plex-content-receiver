accaf836a7e014ad889984f3f8ae2d36da9aa18c
Douglas Batista Santos <dbatists@everis.com>
2020-09-28 15:59:28 -0300

    First commit

---
+++ contentReceiver.js
@@ -0,0 +1,30 @@
+const Request = require('./utils/Request');
+const AdvertisingAnalysis = require('./utils/AdvertisingAnalysis');
+
+exports.handler = async function (event) {
+	try {
+
+		const { content } = event;
+		const obj = {
+			content
+		}
+
+		const { data } = await Request.request(process.env.IMAGE_ANALYZER_API, 'POST', obj);
+		const advertisings = await AdvertisingAnalysis.showContent(data);
+
+		const response = {
+			statusCode: 200,
+			body: advertisings
+		};
+
+		return response;
+	
+	} catch (err) {
+		const reject = {
+			statusCode: 500,
+			body: err
+		}
+
+		throw new Error(JSON.stringify(reject));
+	}
+}
